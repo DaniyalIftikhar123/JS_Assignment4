20f88bbd942d141a1495a30894aff1d4545356d3
DaniyalIftikhar123 <daniyal.iftikhar108@yahoo.com>
2023-09-18 20:03:12 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//***** Chapter 17 Q no 1******
+
+//var arr=[[],[]]
+
+//***** Chapter 17 Q no 2******
+
+// var arr = [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+// for(var i=0; i<arr.length;i++){
+//     for(var j=i;j<arr[i].length;j++){
+//         //arr[i][j]=j
+//     }
+// }
+//document.write(arr[i][j])
+
+//***** Chapter 17 Q no 3******
+// for(var i=1; i<=10;i++){
+//     document.write(i+"<br />")
+// }
+
+//***** Chapter 17 Q no 4******
+// var prtTable= prompt("Enter your number")
+// var tabLen = prompt("Enter Your desire length")
+// document.write("Multiplication Table of "+prtTable+"<br />"+" Length "+ tabLen+"<br />")
+// for(var i=1;i<=tabLen;i++){
+     
+//     //document.write("Multiplication Table of "+prtTable+"<br />"+" Length "+ tabLen)
+//     document.write(prtTable+ "*" +i+" = "+prtTable*i+"<br />")
+// }
+
+//***** Chapter 17 Q no 5******
+
+// var fruits=["Apple","Mango","Banana","Orange","Strawberry"]
+
+// for(var i=0;i<fruits.length;i++){
+//     document.write(fruits[i]+"<br />")
+//     //document.write("Element at index "+ i+fruits[i]+"<br >")
+//      }
+//      document.write("<br />")
+// for(var i=0;i<fruits.length;i++){
+//     document.write("Element at index "+ i + " is " +fruits[i]+"<br >")
+// }
+
+
+//***** Chapter 17 Q no 6******
